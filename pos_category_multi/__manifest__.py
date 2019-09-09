f7da77aa39a28398249b2ee7a8983c729e1b645f
kaadevelop <oleg88rubakov@gmail.com>
2019-09-09 12:26:54 +0500

    :sparkles: change manifest in pos_category_multi

--- pos_category_multi/__manifest__.py
+++ pos_category_multi/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "Point of Sale",
     # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=12.0",
     "images": [],
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
